5e27541405b2a0fb3248752363755e031461000b
s-fukuzawaa <31524675+s-fukuzawaa@users.noreply.github.com>
2019-03-20 09:28:15 -0700

    better for me to rethink about the refficiency from the most successful commit so far. commit to save.

--- Boggle/src/BoggleSolver.java
+++ Boggle/src/BoggleSolver.java
@@ -15,23 +15,19 @@ public class BoggleSolver
     	 this.prefix= new BoggleTrie<Integer>();
     	 for(int i=0; i<dictionary.length; i++)
     	 {
- 			save.put(dictionary[i], 0);
-
-    		if(dictionary[i].length()>=3)
-    		{ 
-    			 for(int k=1; k<=dictionary[i].length(); k++)
-        		 {
-        			 prefix.put(dictionary[i].substring(0, k), 0);
-        		 }
-    		}
     		
+    		 save.put(dictionary[i], 0);
+    		 for(int k=1; k<=dictionary[i].length(); k++)
+    		 {
+    			 prefix.put(dictionary[i].substring(0, k), 0);
+    		 }
     	 }
     	 
     }
     
     private boolean check(String a)
     {
-    	return save.contains(a);
+    	return save.contains(a)&&a.length()>=3;
     }
 
     private ArrayList<String> add(BoggleBoard b, ArrayList<String> valid, boolean[][] marked, String s,int row ,int col)
